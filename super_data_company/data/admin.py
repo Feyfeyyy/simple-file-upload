406da1e1ec86e05473c0f84100b7ed9c1393d439
Feyaaz <feyaazc94@gmail.com>
2024-06-24 19:15:25 +0100

    Update Data Admin

--- super_data_company/data/admin.py
+++ super_data_company/data/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from .models import FileDataRecord, FileDataUpload
 
-admin.site.register(FileDataUpload)
-admin.site.register(FileDataRecord)
+
+@admin.register(FileDataRecord)
+class FileDataRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'data_type', 'data')
+    list_filter = ('data_type',)
+    search_fields = ('upload', 'data_type')
+
+
+@admin.register(FileDataUpload)
+class FileDataUploadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'short_url', 'uploaded_at')
+    list_filter = ('uploaded_at',)
+
+    def short_url(self, obj):
+        return obj.file.url
+
+    short_url.short_description = 'File URL'
